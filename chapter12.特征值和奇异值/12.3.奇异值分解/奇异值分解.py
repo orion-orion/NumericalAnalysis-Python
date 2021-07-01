b654db24566663322bc3853a753a0e32833aef9b
lonelyprince7 <1056692290@qq.com>
2021-07-01 15:54:22 +0800

    add

--- chapter12.特征值和奇异值/12.3.奇异值分解/奇异值分解.py
+++ chapter12.特征值和奇异值/12.3.奇异值分解/奇异值分解.py
@@ -4,17 +4,22 @@ Version: 1.0
 Author: ZhangHongYu
 Date: 2021-06-23 21:25:39
 LastEditors: ZhangHongYu
-LastEditTime: 2021-06-23 22:46:53
+LastEditTime: 2021-06-28 19:10:19
 '''
 import numpy as np
 def svd(A):
     eigen_values, eigen_vectors = np.linalg.eig(A.T.dot(A))
     singular_values = np.sqrt(eigen_values)
+    singular_values = list(reversed(singular_values))
     # 由等式Avi = siui(vi是右奇异向量, ui是左奇异向量)
     # 依次计算左奇异向量
     U = np.zeros((A.shape[0], A.shape[1]))
     for i in range(A.shape[1]):
-        u = A.dot(eigen_vectors[:, i])/singular_values[i]
+        if singular_values[i] != 0:
+            u = A.dot(eigen_vectors[:, i])/singular_values[i]
+        # else:
+        #     u =  np.zeros(A.shape[0])
+        #     u[i] = 1
         U[:, i] = u
     # 给U加上标准正交基去构造R3的基
     for i in range(A.shape[1], A.shape[0]):
@@ -35,18 +40,24 @@ if __name__ == '__main__':
     #     ]
     # )
     # 例二：方阵
-    A = np.array(
-       [
-           [0, 1],
-           [0, -1]
-       ]
-    )
-    # 例三：对称矩阵
     # A = np.array(
-    #     [
-    #         [0, 1],
-    #         [1, 3/2]
-    #     ]
+    #    [
+    #        [0, 1],
+    #        [0, -1]
+    #    ]
     # )
+    # 例三：对称矩阵
+    A = np.array(
+        [
+            [0, 1],
+            [1, 3/2]
+        ]
+    )
     U, S, V = svd(A)
     print(U, "\n\n", S, "\n\n", V)
+
+    print("\n\n\n")
+
+    # 调用api核对
+    U2, S2, V2 = np.linalg.svd(A)
+    print(U2, "\n\n", S2, "\n\n", V2)
