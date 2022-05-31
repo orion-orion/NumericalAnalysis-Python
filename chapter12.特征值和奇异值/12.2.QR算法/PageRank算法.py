19681ac6d821ec0dcc66e1091af4de2652150776
orion-orion <orion-orion@foxmail.com>
2022-05-31 21:23:15 +0800

    update pangerank

--- chapter12.特征值和奇异值/12.2.QR算法/PageRank算法.py
+++ chapter12.特征值和奇异值/12.2.QR算法/PageRank算法.py
@@ -4,7 +4,7 @@ Version: 1.0
 Author: ZhangHongYu
 Date: 2021-06-23 15:42:51
 LastEditors: ZhangHongYu
-LastEditTime: 2021-07-08 20:45:32
+LastEditTime: 2022-05-31 21:15:46
 '''
 import numpy as np
 # 归一化同时迭代，k是迭代步数
@@ -12,14 +12,14 @@ import numpy as np
 def PageRank(A, p, k, q):
     assert(A.shape[0]==A.shape[1])
     n = A.shape[0]
-    M = A.copy().astype(np.float32) #注意要转为浮点型
+    M = A.T.astype(np.float32) #注意要转为浮点型
     for i in range(n):
-        M[i, :] = M[i, :]/np.sum(M[i, :])
+        M[:, i] = M[:, i]/np.sum(M[:, i])
     G = (q/n)*np.ones((n,n)) + (1-q)*M
-    G_T = G.T
+    #G_T = G.T
     p_t = p.copy()
     for i in range(k):
-        y = G_T.dot(p_t)
+        y = G.dot(p_t)
         p_t = y/np.max(y)
     return p_t/np.sum(p_t)
 if __name__ == '__main__':
